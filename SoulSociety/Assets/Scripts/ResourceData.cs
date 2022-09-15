d0eb4244f54b4ac4a9785c15a47369a12caf2871
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-09-15 15:03:30 +0900

    Add scene
    Lobby Scene,
    Game Scene

---
+++ SoulSociety/Assets/Scripts/ResourceData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New ResourceData", menuName = "ScriptableObjects/ResourceData", order = 5)]
+public class ResourceData : ScriptableObject
+{
+    public GameObject effData1;
+
+}
+
