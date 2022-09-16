18cdbe49dcbf73aa5ddf3959423f2f1ac3ad395a
chaeza <96408273+chaeza@users.noreply.github.com>
2022-09-16 17:18:55 +0900

    로비구현중
    Lobby

--- SoulSociety/Assets/Scripts/NetworkManager.cs
+++ SoulSociety/Assets/Scripts/NetworkManager.cs
@@ -10,26 +10,60 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public InputField NickNameInput;
     public GameObject DisconnectPanel;
+    public GameObject RobbyPanel;
 
+    [SerializeField] Button btnConnect = null;
+
+    public Text nickName = null;
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, false);
         PhotonNetwork.SendRate = 60;
         PhotonNetwork.SerializationRate = 30;
-    }
 
-    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+    }
+    private void Start()
+    {
+        btnConnect.interactable = false; // 버튼 입력 막기
+        RobbyPanel.SetActive(false);
+        //마스터 서버 접속 요청
+        PhotonNetwork.ConnectUsingSettings(); //Photon.Pun 내부 클래스
+
+    }
 
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, null);
+        btnConnect.interactable = true;
+        Debug.Log("## OnConnected to Master");
 
     }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        btnConnect.interactable = false;
+        RobbyPanel.SetActive(false);
+    }
+    // 이름 입력 컨트롤(inputField)
+    public void OnEndEdit(string instr)
+    {
+        PhotonNetwork.NickName = instr; //닉네임 할당
+    }
 
+    public void OnClick_Connected()
+    {
+        if (string.IsNullOrEmpty(PhotonNetwork.NickName) == true)
+            return;
+
+        Debug.Log("dd");
+        PhotonNetwork.JoinOrCreateRoom("myroom", new RoomOptions { MaxPlayers = 4 }, null);
+        DisconnectPanel.SetActive(false);
+        RobbyPanel.SetActive(true);
+    }
     public override void OnJoinedRoom()
     {
-        DisconnectPanel.SetActive(false);
+        PhotonNetwork.LocalPlayer.NickName = nickName.text;
+
+
     }
 
     private void Update()
@@ -38,9 +72,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         
     }
 
-    public override void OnDisconnected(DisconnectCause cause)
-    {
-        DisconnectPanel.SetActive(true);
-    }
+    
 
 }
