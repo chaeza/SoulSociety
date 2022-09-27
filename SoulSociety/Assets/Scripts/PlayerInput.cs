4b55e105271d0697a73759adc49942c405e7fab5
woobong1 <111822994+woobong1@users.noreply.github.com>
2022-09-27 12:11:02 +0900

    woojin test

--- SoulSociety/Assets/Scripts/PlayerInput.cs
+++ SoulSociety/Assets/Scripts/PlayerInput.cs
@@ -6,6 +6,8 @@ public class PlayerInput : MonoBehaviour
 {
     //이 스크립트는 플레이어에게 
     public KeyCode inputKey { get; private set; }
+    public KeyCode Esc { get; private set; }
+    bool escDown;
     void Update()
     {
     
@@ -20,12 +22,21 @@ public class PlayerInput : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.D)) inputKey = KeyCode.D;
         else if (Input.GetKeyDown(KeyCode.F)) inputKey = KeyCode.F;
         else if (Input.GetKey(KeyCode.Tab)) inputKey = KeyCode.Tab;
-        else if (Input.GetKey(KeyCode.Escape)) inputKey = KeyCode.Escape;
         else if (Input.GetKeyDown(KeyCode.Alpha2)) inputKey = KeyCode.Alpha2;
         else if (Input.GetKeyDown(KeyCode.Alpha1)) inputKey = KeyCode.Alpha1;
         else inputKey = KeyCode.Alpha0;
+        if (Input.GetKeyDown(KeyCode.Escape) && escDown == false)
+        {
+            Esc = KeyCode.Escape;
+            escDown = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            escDown = false;
+            Esc = KeyCode.Alpha0;
+        }
 
-    //    Debug.Log(inputKey);
-        
+        //    Debug.Log(inputKey);
+
     }
 }
