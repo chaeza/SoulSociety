b71a25e1749fd536741b470a3ada7b0f0c9cfa5a
woobong1 <111822994+woobong1@users.noreply.github.com>
2022-09-27 17:04:41 +0900

    Woojin_Skill
    스킬입력중

--- SoulSociety/Assets/Scripts/RandomSkill.cs
+++ SoulSociety/Assets/Scripts/RandomSkill.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class RandomSkill : MonoBehaviour
 {
-    int skillNum = 2;//ÃÑ ½ºÅ³ °¹¼ö
+    int skillNum = 5;//ÃÑ ½ºÅ³ °¹¼ö
     public int skillRan { get; set; } = 0;//·£´ýÀ¸·Î »ÌÀ» ½ºÅ³ ¹øÈ£
     public void GetRandomSkill(GameObject player)// ·£´ý½ºÅ³ Áö±Þ
     {
         skillRan = Random.Range(1, skillNum + 1);//½ºÅ³¹øÈ£ »Ì±â
         if (skillRan == 1) player.AddComponent<Skill>();
-        if (skillRan == 2) player.AddComponent<Skill2>();
+        else if (skillRan == 2) player.AddComponent<Skill2>();
+        else if (skillRan == 3) player.AddComponent<StoneField>();
+        else if (skillRan == 4) player.AddComponent<SpearCrash>();
+        else if (skillRan == 5) player.AddComponent<SwordRain>();
         GameMgr.Instance.uIMgr.SkillUI(skillRan);
     }
     
