3192b3f5e09f7b3e925ef1b4860c88d265d0ceb6
chaeza <96408273+chaeza@users.noreply.github.com>
2022-10-06 10:30:38 +0900

    soya

--- SoulSociety/Assets/Scripts/metaAPI/MetaTrendAPI.cs
+++ SoulSociety/Assets/Scripts/metaAPI/MetaTrendAPI.cs
@@ -44,7 +44,7 @@ public class MetaTrendAPI : MonoBehaviour
 			{
 				Debug.Log("## " + response.ToString());
 				res_UserProfile = response;
-				Debug.Log(res_UserProfile.userProfile.username);
+				Debug.Log("µ¿Çª" +res_UserProfile.userProfile.username);
 			}
 		});
 	}
