23cb97cf1a9cc147794e9fa822252ece15413e4a
chaeza <96408273+chaeza@users.noreply.github.com>
2022-10-06 21:37:00 +0900

    soya

--- SoulSociety/Assets/Scripts/Skills/woojin/ControlMissie.cs
+++ SoulSociety/Assets/Scripts/Skills/woojin/ControlMissie.cs
@@ -116,11 +116,13 @@ public class ControlMissie : MonoBehaviourPun
         yield return new WaitForSeconds(time);
         GameObject a = PhotonNetwork.Instantiate("ControlMissie", desiredDir, Quaternion.identity);//이펙트를 포톤 인스턴스를 합니다.
         a.AddComponent<ControlMissieHit>();//이펙트에 히트 스크립트를 넣습니다.
+        sound = a.GetComponent<AudioSource>();
+        StartCoroutine(soundCh());
+        sound.Play();
         a.SendMessage("AttackerName", gameObject.GetPhotonView().ViewID, SendMessageOptions.DontRequireReceiver);//이펙트에 공격자를 지정합니다.
         a.transform.Rotate(-90, 0, 0);
         yield return new WaitForSeconds(4.5f);    //콜라이더 킴
         a.GetComponent<SphereCollider>().enabled = true;
-
         // sound = a.GetComponent<AudioSource>();
         //  StartCoroutine(soundCh());
         // sound.Play();
@@ -133,7 +135,7 @@ public class ControlMissie : MonoBehaviourPun
 
     IEnumerator soundCh()
     {
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(9f);
         sound.Stop();
     }
 }
