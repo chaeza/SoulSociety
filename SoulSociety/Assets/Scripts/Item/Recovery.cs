06f93403609c8ade01fd2d55c89bd3bba53a86fd
chaeza <96408273+chaeza@users.noreply.github.com>
2022-10-27 20:24:59 +0900

    Soya_1027
    cleaning

--- SoulSociety/Assets/Scripts/Item/Recovery.cs
+++ SoulSociety/Assets/Scripts/Item/Recovery.cs
@@ -3,42 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Recovery : MonoBehaviourPun, ItemMethod//아이템 인터페이스 상속
+//Item interface inheritance
+public class Recovery : MonoBehaviourPun, ItemMethod
 {
     [SerializeField]
-    int itemNum = 0;
-    AudioSource sound;
+    private int itemNum = 0;
+    private AudioSource sound;
 
-    public void GetItem(int itemnum)//해당 아이템이 어느 인벤토리에 있는지 순서 책정
+    //Order which inventory the item is in
+    public void GetItem(int itemnum)
     {
         if (itemNum == 0)
             itemNum = itemnum;
     }
 
-    public void ItemFire()//플레이어 Attack에서 키입력시 해당 아이템의 인벤토리 위치를 비교하여 아이템 스킬 사용 
+    //Use item skill by comparing the inventory location of the item when keying in player attack
+    public void ItemFire()
     {
         if (itemNum == 1 && GameMgr.Instance.playerInput.inputKey == KeyCode.Q) ItemSkill();
         else if (itemNum == 2 && GameMgr.Instance.playerInput.inputKey == KeyCode.W) ItemSkill();
         else if (itemNum == 3 && GameMgr.Instance.playerInput.inputKey == KeyCode.E) ItemSkill();
         else if (itemNum == 4 && GameMgr.Instance.playerInput.inputKey == KeyCode.R) ItemSkill();
     }
-    public void ItemSkill()//이 아이템이 가지고 있는 아이템 스킬 사용
+
+    //Use the item skill an item has
+    public void ItemSkill()
     {
-        // 스킬 구현
+        //Skill Implementation
+        GameObject recovery = PhotonNetwork.Instantiate("Recovery", transform.position, Quaternion.identity);
+        recovery.AddComponent<MyPosition>();
+        recovery.SendMessage("MyPos", gameObject.transform, SendMessageOptions.DontRequireReceiver);
+        recovery.SendMessage("YPos", 2f, SendMessageOptions.DontRequireReceiver);
 
-        GameObject a = PhotonNetwork.Instantiate("Recovery", transform.position, Quaternion.identity);//이펙트를 포톤 인스턴스를 합니다.
-        a.AddComponent<MyPosition>();
-        a.SendMessage("MyPos", gameObject.transform, SendMessageOptions.DontRequireReceiver);
-        a.SendMessage("YPos", 2f, SendMessageOptions.DontRequireReceiver);
-
-        sound = a.GetComponent<AudioSource>();
+        //Attach audio source to recovery item
+        sound = recovery.GetComponent<AudioSource>();
         sound.Play();
 
         gameObject.GetPhotonView().RPC("ChageHP", RpcTarget.All, 30f);
-        GameMgr.Instance.DestroyTarget(a, 2f);
-        //
+        GameMgr.Instance.DestroyTarget(recovery, 2f);
+
         GameMgr.Instance.uIMgr.UseItem(itemNum);
-        GameMgr.Instance.inventory.RemoveInventory(itemNum);//인벤토리에서 이 스킬을 소유한 것을 초기화함
-        Destroy(GetComponent<Recovery>());//해당 아이템을 사용후 컴포넌트 삭제
+        //Remove possession of this skill in inventory
+        GameMgr.Instance.inventory.RemoveInventory(itemNum);
+        //Delete the component after using the item
+        Destroy(GetComponent<Recovery>());
     }
 }
--- SoulSociety/Assets/Scripts/Scene/LoadingText.cs
+++ SoulSociety/Assets/Scripts/Scene/LoadingText.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 
 public class LoadingText : MonoBehaviour
 {
-    public Text[] text;
-    public RawImage[] image;
+    private int ran;
+    private int imageRan;
 
-    int ran;
-    int imageRan;
+    //로딩텍스트를 넣는 배열
+    public Text[] text;
+    //Array of images to be displayed on the screen when loading
+    public RawImage[] image;
 
     private void Start()
     {
         ran = Random.Range(0, text.Length);
         imageRan = Random.Range(0, image.Length);
 
+        //Activate the gameObject based on a random number.
         text[ran].gameObject.SetActive(true);
         image[imageRan].gameObject.SetActive(true);
 
