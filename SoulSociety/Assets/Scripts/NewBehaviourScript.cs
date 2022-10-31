418d5a05e66cef655d9dda85c163d72eb61057cb
KingOneChance <111823096+KingOneChance@users.noreply.github.com>
2022-10-31 19:16:47 +0900

    Soya_1chacne
    Backup

---
+++ SoulSociety/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class FollowCam : MonoBehaviour
+{
+    private void Awake()
+    {
+        a = 3;
+    }
+}
